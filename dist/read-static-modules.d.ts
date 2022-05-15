8bb25aaee90dcfbd0ea457bf6677989e260b3e5f
Eugene Tseitlin <eugene@amplication.com>
2022-05-15 13:18:10 +0300

    remove dist and package name

--- dist/read-static-modules.d.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "./types/module";
-/**
- * Reads files from given source directory and maps them to module objects with
- * path relative to given basePath
- * @param source source directory to read files from
- * @param basePath path to base the created modules path on
- * @returns array of modules
- */
-export declare function readStaticModules(source: string, basePath: string): Promise<Module[]>;
--- dist/types/module.d.ts
+++
@@ -1,4 +0,0 @@
-export declare type Module = {
-    path: string;
-    code: string;
-};
